a3619270ca074125a7a92c71822a75d3e525952a
Juan Felix <andrade.juan@outlook.com.br>
2023-03-02 23:11:49 -0300

    feat: add get method with use-cases features in api/highways

--- pages/api/highways.ts
+++ pages/api/highways.ts
@@ -1,6 +1,8 @@
 import { Highway } from '@/src/application/entities/highway'
 import { CreateHighway } from '@/src/application/use-cases/create-highway'
 import { HighwayValidationError } from '@/src/application/use-cases/errors/highway-validation-error'
+import { FindHighwaysByName } from '@/src/application/use-cases/find-highways-by-name'
+import { GetAllHighways } from '@/src/application/use-cases/get-all-highways'
 import {
   DataMappingService,
   Filenames
@@ -9,6 +11,7 @@ import { DataMappingHighwaysRepository } from '@/src/infra/data-mapping/reposito
 import { createHighwayBody } from '@/src/infra/http/dtos/create-highway-body'
 import { Controller } from '@/src/utils/controller'
 import { NextApiRequest, NextApiResponse } from 'next'
+import RenderResult from 'next/dist/server/render-result'
 
 namespace HighwaysController {
   export namespace Post {
@@ -22,36 +25,50 @@ class HighwaysController extends Controller {
   private readonly dataMappingHighwaysRepository =
     new DataMappingHighwaysRepository(this.dataMappingService)
 
+  private readonly findHighwaysByName = new FindHighwaysByName(
+    this.dataMappingHighwaysRepository
+  )
+
   private readonly createHighway = new CreateHighway(
     this.dataMappingHighwaysRepository
   )
 
+  private readonly getAllHighways = new GetAllHighways(
+    this.dataMappingHighwaysRepository
+  )
+
   async get(req: Controller.Request, res: Controller.Response) {
-    const { q } = req.query as { q: string }
+    try {
+      const { q: searchQuery } = req.query as { q: string }
 
-    if (typeof q !== 'string') {
-      return res.status(400).send({
-        error: 'query text must be provided with `q` param'
-      })
+      console.log(searchQuery, !searchQuery)
+
+      if (!searchQuery) {
+        const { highways: allHighwaysList } =
+          await this.getAllHighways.execute()
+
+        return res.status(200).send(allHighwaysList)
+      }
+
+      const { highways: highwaysFetchedByNameList } =
+        await this.findHighwaysByName.execute({
+          highwayName: searchQuery
+        })
+
+      return res.status(200).send(highwaysFetchedByNameList)
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        throw error
+      }
+      const { message } = error
+
+      switch (message) {
+        case 'No highway was found':
+          return res.status(404).send({ error: 'No highway was found' })
+        default:
+          return res.status(400).send({ error: message })
+      }
     }
-
-    const highwayList: Highway[] = await this.dataMappingService.loadFromName(
-      Filenames.Highways
-    )
-    const queryTextInLowercase = q.toLowerCase()
-
-    const highwayFilteredByTextList = highwayList.filter(
-      highway =>
-        highway.code.toLowerCase().includes(queryTextInLowercase) ||
-        highway.concessionaireName
-          ?.toLowerCase()
-          // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-          .includes(queryTextInLowercase) ||
-        highway.name.toLowerCase().includes(queryTextInLowercase) ||
-        highway.description.toLowerCase().includes(queryTextInLowercase)
-    )
-
-    return res.status(200).send(highwayFilteredByTextList)
   }
 
   async post(
