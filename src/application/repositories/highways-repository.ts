ad6db6f29588c00c47ca039a361019777442250e
Juan Felix <andrade.juan@outlook.com.br>
2023-03-02 21:34:40 -0300

    chore: add getAll method in abstract class highays-repository

--- src/application/repositories/highways-repository.ts
+++ src/application/repositories/highways-repository.ts
@@ -2,7 +2,8 @@ import { Highway } from '@/src/application/entities/highway'
 
 export abstract class HighwaysRepository {
   abstract create(highway: Partial<Highway>): Promise<{ id: number }>
-  abstract findByName(highwayName: string): Promise<Highway[]>
+  abstract findByName(highwayName?: string): Promise<Highway[]>
+  abstract getAll(): Promise<Highway[]>
   abstract findById(highwayId: number): Promise<Highway | null>
   abstract save(highwayModified: Highway): Promise<void>
 }
