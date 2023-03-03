30a7c355aea10abe91391d047a9b086f3b57d3a2
Juan Felix <andrade.juan@outlook.com.br>
2023-03-03 17:08:54 -0300

    feat: add deleting and editing features to highways module page

--- pages/gerenciamento/rodovias.tsx
+++ pages/gerenciamento/rodovias.tsx
@@ -1,21 +1,17 @@
-import * as InsertNewHighwayModal from '@/src/components/Modal'
+import { Highway as HighwayZod } from '@/src/infra/http/dtos/highway-schema'
 import Head from 'next/head'
 import * as Footer from '../../src/components/Footer'
 import * as FormSearch from '../../src/components/FormSearch'
 import * as Header from '../../src/components/Header'
 import * as RodGrid from '../../src/components/RodGrid'
 
+import { Highway } from '@/src/application/entities/highway'
+import InsertNewHighwayModal from '@/src/components/Highways/InserNewHighwayModal'
 import { useCallback, useEffect, useState } from 'react'
 import RodImg from '../../assets/rod.png'
-import { useForm, useWatch } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Highway } from '@/src/application/entities/highway'
-
-import {
-  Highway as HighwayZod,
-  highwaySchema
-} from '@/src/infra/http/dtos/highway-schema'
 import { useRouter } from 'next/router'
+import EditHighwayModal from '@/src/components/Highways/EditHighwayModal'
+import Link from 'next/link'
 
 let TIMEOUT_QUEUE_REF: any | null = null
 let LAST_SEARCHED_TEXT = ''
@@ -23,24 +19,12 @@ let LAST_SEARCHED_TEXT = ''
 export default function Rodovias() {
   const [highways, setHighways] = useState<Highway[]>([])
   const [toggleInsertHighway, setToggleInsertHighway] = useState(false)
+  const [toggleEditHighway, setToggleEditHighway] = useState(false)
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    reset,
-    formState: { errors },
-    control
-  } = useForm<HighwayZod>({
-    resolver: zodResolver(highwaySchema)
-  })
+  const router = useRouter()
 
-  const { hasConcessionaire } = useWatch({
-    control,
-    defaultValue: {
-      hasConcessionaire: false
-    }
-  })
+  const highwayId = router.query.id as string | undefined
+  const selectedHighway = highways.find(({ id }) => id === Number(highwayId))
 
   function handleSearchHighways(searchText = '') {
     fetch(`/api/highways?q=${searchText}`)
@@ -55,18 +39,6 @@ export default function Rodovias() {
         setHighways(highwaysFetched)
       })
       .catch(console.error)
-  }
-
-  function handleResetFormData() {
-    reset({
-      code: '',
-      concessionaireLink: null,
-      concessionaireName: null,
-      description: '',
-      emergencyContacts: '',
-      hasConcessionaire: false,
-      name: ''
-    })
   }
 
   function handleSubmitCreateHighway({
@@ -100,10 +72,9 @@ export default function Rodovias() {
         }
 
         if (createHighwayResponse.status === 201) {
-          setToggleInsertHighway(false) // hidde modal
+          setToggleInsertHighway(false) // hidden modal
           alert('Rodovia salva com sucesso')
           handleSearchHighways('') // refresh list view
-          handleResetFormData()
           return
         }
 
@@ -112,6 +83,81 @@ export default function Rodovias() {
         )
       })
       .catch(console.warn)
+  }
+
+  function handleSubmitEditHighway({
+    id,
+    code,
+    concessionaireLink,
+    concessionaireName,
+    description,
+    emergencyContacts,
+    hasConcessionaire,
+    name
+  }: HighwayZod & { id: number }) {
+    fetch(`/api/highways/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        code,
+        concessionaireLink,
+        concessionaireName,
+        description,
+        emergencyContacts,
+        hasConcessionaire,
+        name
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then(editHighwayResponse => {
+        if (editHighwayResponse.status >= 400) {
+          alert('Erro ao salvar a rodovia')
+          return
+        }
+
+        if (editHighwayResponse.status === 200) {
+          setToggleEditHighway(false) // hidden modal
+          alert('Dados salvos com sucesso')
+          handleSearchHighways('') // refresh list view
+          return
+        }
+
+        alert(
+          'Ocorreu um erro desconhecido durante o processo de alterar a rodovia no sistema.'
+        )
+      })
+      .catch(console.warn)
+  }
+
+  function handleSubmitDeleteHighway({ id }: { id: number }) {
+    const confirmation = confirm('Deseja realmente excluir essa rodovia?')
+
+    if (!confirmation) {
+      return
+    }
+
+    fetch(`/api/highways/${id}`, {
+      method: 'DELETE'
+    }).then(deleteHighwayResponse => {
+      if (deleteHighwayResponse.status >= 400) {
+        alert(
+          `Ocorreu um erro durante a operação de exclusão da rodovia, tente novamente ou procure uma solução para o problema. Status [${deleteHighwayResponse.status}]`
+        )
+        return
+      }
+
+      if (deleteHighwayResponse.status === 200) {
+        alert('Rodovia excluída com sucesso!')
+        setToggleEditHighway(false)
+        handleSearchHighways('')
+        return
+      }
+
+      alert(
+        'Ocorreu um erro desconhecido durante o processo de alterar a rodovia no sistema.'
+      )
+    })
   }
 
   const handleOnChangeInputSearch = useCallback((text = '') => {
@@ -131,205 +177,35 @@ export default function Rodovias() {
   }, [])
 
   useEffect(() => {
-    handleSearchHighways('')
-  }, [])
+    setToggleEditHighway(Boolean(highwayId) && Boolean(selectedHighway))
+  }, [highwayId])
 
   useEffect(() => {
-    if (!hasConcessionaire) {
-      reset({
-        concessionaireLink: null,
-        concessionaireName: null
-      })
-    }
-  }, [hasConcessionaire])
+    handleSearchHighways('')
+  }, [])
 
   return (
     <div className="min-h-screen w-full bg-white">
       <Head>
         <title>Proteus - Mapeamento de Rodovias</title>
       </Head>
-      <InsertNewHighwayModal.Root show={toggleInsertHighway}>
-        <InsertNewHighwayModal.Mask />
-        <InsertNewHighwayModal.ModalContainer
-          onSubmit={
-            handleSubmit(
-              d => handleSubmitCreateHighway(d),
-              errors => console.log(errors)
-            ) as any
-          }
-          onReset={() => {
-            handleResetFormData()
-            setToggleInsertHighway(false)
-          }}
-          className="pl-10 pr-[100px]"
-        >
-          <InsertNewHighwayModal.Title>
-            Inserir Rodovia
-          </InsertNewHighwayModal.Title>
-          <InsertNewHighwayModal.ContainerField>
-            <label htmlFor="">Nome</label>
-            <input
-              {...register('name')}
-              type="text"
-              autoFocus
-              placeholder="Informe o nome da rodovia"
-              className={`
-                  my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40
-                  ${
-                    (errors.name?.message &&
-                      `border-red-400 focus:border-red-400 focus:ring-red-400`) ??
-                    ''
-                  }
-                `}
-            />
-            {errors.name?.message && (
-              <p className="max-w-xs text-sm italic text-red-500">
-                {errors.name.message}
-              </p>
-            )}
-          </InsertNewHighwayModal.ContainerField>
-          <InsertNewHighwayModal.ContainerField>
-            <label htmlFor="">Código</label>
-            <input
-              {...register('code')}
-              type="text"
-              placeholder="BR-000"
-              className={`
-                  my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40
-                  ${
-                    (errors.code?.message &&
-                      `border-red-400 focus:border-red-400 focus:ring-red-400`) ??
-                    ''
-                  }
-                `}
-            />
-            {errors.code?.message && (
-              <p className="max-w-xs text-sm italic text-red-500">
-                {errors.code.message}
-              </p>
-            )}
-          </InsertNewHighwayModal.ContainerField>
-          <InsertNewHighwayModal.ContainerField>
-            <label htmlFor="">Descrição</label>
-            <textarea
-              {...register('description')}
-              title="description"
-              placeholder="..."
-              rows={2}
-              className={`
-                  my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40
-                  ${
-                    (errors.description?.message &&
-                      `border-red-400 focus:border-red-400 focus:ring-red-400`) ??
-                    ''
-                  }
-                `}
-            />
-            {errors.description?.message && (
-              <p className="max-w-xs text-sm italic text-red-500">
-                {errors.description.message}
-              </p>
-            )}
-          </InsertNewHighwayModal.ContainerField>
-          <InsertNewHighwayModal.ContainerField>
-            <div className="flex w-fit items-center">
-              <input
-                placeholder="Selecione"
-                type="checkbox"
-                className="my-2 mr-2 cursor-pointer rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                {...register('hasConcessionaire')}
-              />
-              <label htmlFor="">Possui concessionária?</label>
-            </div>
-          </InsertNewHighwayModal.ContainerField>
-          {hasConcessionaire && (
-            <>
-              <InsertNewHighwayModal.ContainerField hidden={!hasConcessionaire}>
-                <label htmlFor="">Nome concessionária</label>
-                <input
-                  type="text"
-                  {...register('concessionaireName')}
-                  placeholder="Informe o nome da concessionária"
-                  className={`
-                  my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40
-                  ${
-                    (errors.concessionaireName?.message &&
-                      `border-red-400 focus:border-red-400 focus:ring-red-400`) ??
-                    ''
-                  }
-                `}
-                />
-                {errors.concessionaireName?.message && (
-                  <p className="max-w-xs text-sm italic text-red-500">
-                    {errors.concessionaireName.message}
-                  </p>
-                )}
-              </InsertNewHighwayModal.ContainerField>
-              <InsertNewHighwayModal.ContainerField hidden={!hasConcessionaire}>
-                <label htmlFor="">Link externo da concessionária</label>
-                <input
-                  type="text"
-                  {...register('concessionaireLink')}
-                  placeholder="https://"
-                  className={`
-                  my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40
-                  ${
-                    (errors.concessionaireLink?.message &&
-                      `border-red-400 focus:border-red-400 focus:ring-red-400`) ??
-                    ''
-                  }
-                `}
-                />
-                {errors.concessionaireLink?.message && (
-                  <p className="max-w-xs text-sm italic text-red-500">
-                    {errors.concessionaireLink.message}
-                  </p>
-                )}
-              </InsertNewHighwayModal.ContainerField>
-            </>
-          )}
-          <InsertNewHighwayModal.ContainerField hidden={!hasConcessionaire}>
-            <label htmlFor="">Descrição contatos de emergência</label>
-            <textarea
-              title="description"
-              placeholder="..."
-              {...register('emergencyContacts')}
-              rows={2}
-              className={`
-                  my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40
-                  ${
-                    (errors.emergencyContacts?.message &&
-                      `border-red-400 focus:border-red-400 focus:ring-red-400`) ??
-                    ''
-                  }
-                `}
-            />
-            {errors.emergencyContacts?.message && (
-              <p className="max-w-xs text-sm italic text-red-500">
-                {errors.emergencyContacts.message}
-              </p>
-            )}
-          </InsertNewHighwayModal.ContainerField>
 
-          <InsertNewHighwayModal.ContainerField>
-            <div className="flex">
-              <FormSearch.Button
-                type="submit"
-                className="mx-0 bg-green-600 text-white hover:bg-green-500 focus:ring-green-600 sm:mx-0"
-              >
-                Salvar
-              </FormSearch.Button>
-              <FormSearch.Button
-                type="reset"
-                className="mx-2 bg-gray-500 text-white hover:bg-gray-400 focus:ring-gray-500"
-              >
-                Cancelar
-              </FormSearch.Button>
-            </div>
-          </InsertNewHighwayModal.ContainerField>
-        </InsertNewHighwayModal.ModalContainer>
-      </InsertNewHighwayModal.Root>
-
+      <InsertNewHighwayModal
+        handleOnReset={() => setToggleInsertHighway(false)}
+        handleOnSubmit={handleSubmitCreateHighway}
+        show={toggleInsertHighway}
+      />
+      <EditHighwayModal
+        handleOnDelete={handleSubmitDeleteHighway}
+        handleOnSubmit={handleSubmitEditHighway}
+        handleOnReset={() => {
+          router.push('/gerenciamento/rodovias') // reset
+          setToggleEditHighway(false)
+        }}
+        highwayData={selectedHighway}
+        highwayId={highwayId ? Number(highwayId) : undefined}
+        show={toggleEditHighway}
+      />
       <div className="container relative mx-auto grid min-h-screen grid-rows-[60px,auto,70px] px-6 pb-8 pt-4">
         <Header.Root>
           <Header.NavLinksContainer>
@@ -379,17 +255,19 @@ export default function Rodovias() {
           <div className="flex w-full flex-1">
             <RodGrid.Root>
               {highways.map(highway => (
-                <RodGrid.CardContainer
+                <Link
                   key={highway.id}
-                  href={`/rodovias/${highway.code}`}
+                  href={`/gerenciamento/rodovias?id=${highway.id}`}
                 >
-                  <RodGrid.CardImage src={RodImg.src} />
-                  <RodGrid.CardTitle>{highway.name}</RodGrid.CardTitle>
-                  <RodGrid.CardDescription>
-                    {highway.description}
-                  </RodGrid.CardDescription>
-                  <RodGrid.LinkIcon />
-                </RodGrid.CardContainer>
+                  <RodGrid.CardContainer href={`/rodovias/${highway.code}`}>
+                    <RodGrid.CardImage src={RodImg.src} />
+                    <RodGrid.CardTitle>{highway.name}</RodGrid.CardTitle>
+                    <RodGrid.CardDescription>
+                      {highway.description}
+                    </RodGrid.CardDescription>
+                    <RodGrid.LinkIcon />
+                  </RodGrid.CardContainer>
+                </Link>
               ))}
             </RodGrid.Root>
           </div>
