8dae37736ed69876dc6c80e81e4a1d15b3080675
Juan Felix <andrade.juan@outlook.com.br>
2023-03-02 22:34:25 -0300

    fix: get-all-highways use-case to use response object pattern

--- src/application/use-cases/get-all-highways.ts
+++ src/application/use-cases/get-all-highways.ts
@@ -1,21 +1,29 @@
 import { Highway } from '../entities/highway'
 import { HighwaysRepository } from '../repositories/highways-repository'
 
+namespace GetAllHighways {
+  export interface Response {
+    highways: Highway[]
+  }
+}
+
 export class GetAllHighways {
   constructor(private readonly highwaysRepository: HighwaysRepository) {}
 
-  async execute(): Promise<Highway[]> {
+  async execute(): Promise<GetAllHighways.Response> {
     const highways = await this.highwaysRepository.getAll()
 
-    return highways.map(highway => ({
-      code: highway.code,
-      description: highway.description,
-      emergencyContacts: highway.emergencyContacts,
-      hasConcessionaire: highway.hasConcessionaire,
-      id: highway.id,
-      name: highway.name,
-      concessionaireLink: highway.concessionaireLink,
-      concessionaireName: highway.concessionaireName
-    }))
+    return {
+      highways: highways.map(highway => ({
+        code: highway.code,
+        description: highway.description,
+        emergencyContacts: highway.emergencyContacts,
+        hasConcessionaire: highway.hasConcessionaire,
+        id: highway.id,
+        name: highway.name,
+        concessionaireLink: highway.concessionaireLink,
+        concessionaireName: highway.concessionaireName
+      }))
+    }
   }
 }
