8b3ff8f0b747d47416c34591daba3678d53f0fc7
Juan Felix <andrade.juan@outlook.com.br>
2023-03-03 21:25:55 -0300

    chore: parts page adaptation to use route fetch highways

--- pages/gerenciamento/trechos/index.tsx
+++ pages/gerenciamento/trechos/index.tsx
@@ -6,25 +6,26 @@ import * as Header from '../../../src/components/Header'
 
 import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { Highway } from '../../../src/application/entities/highway'
-import highways from '../../../src/data/highways.json'
 
-interface Props {
-  highways: Highway[]
-}
-
-export const getServerSideProps: GetServerSideProps<Props> = async ctx => {
-  return {
-    props: {
-      highways
-    }
-  }
-}
-
-export default function Trechos({ highways }: Props) {
+export default function Trechos() {
   const [selectedHighway, setSelectedHighway] = useState<Highway | null>(null)
   const router = useRouter()
+  const [highways, setHighways] = useState<Highway[]>([])
+
+  useEffect(() => {
+    fetch('/api/highways').then(async fetchHighwaysResponse => {
+      if (fetchHighwaysResponse.status >= 400) {
+        alert('Ocorreu um erro durante o carregamento das rodovias.')
+        return
+      }
+
+      const highwaysFetched: Highway[] = await fetchHighwaysResponse.json()
+
+      setHighways(highwaysFetched)
+    })
+  }, [])
 
   function handleOnChangeSelectedHighway(
     event: ChangeEvent<HTMLSelectElement>
@@ -48,6 +49,50 @@ export default function Trechos({ highways }: Props) {
       <Head>
         <title>Proteus - Mapeamento de Trechos</title>
       </Head>
+      <SelectHighwayModal.Root show>
+        <SelectHighwayModal.Mask />
+        <SelectHighwayModal.ModalContainer>
+          <SelectHighwayModal.Title>
+            Rodovia a ser Mapeada
+          </SelectHighwayModal.Title>
+          <SelectHighwayModal.ContainerField>
+            <label htmlFor="">Nome da Rodovia</label>
+            <select
+              placeholder="Selecione"
+              className="my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+              onChange={handleOnChangeSelectedHighway}
+            >
+              <option value="" hidden defaultValue="true">
+                Selecione uma opção
+              </option>
+              {highways.map(highway => (
+                <option key={highway.id} value={highway.id}>
+                  {highway.name}
+                </option>
+              ))}
+            </select>
+          </SelectHighwayModal.ContainerField>
+          <SelectHighwayModal.ContainerField>
+            <FormSearch.Button
+              type="button"
+              disabled={!hasHighwaySelected}
+              className="
+                disabled:focus-ring-white
+                  mx-0 flex
+                justify-center bg-green-600
+                text-white
+                  hover:bg-green-500 focus:ring-green-600
+                disabled:bg-green-300
+                disabled:hover:bg-green-300
+                  sm:mx-0
+                "
+              onClick={handleToInitializeMapping}
+            >
+              Começar a Mapear
+            </FormSearch.Button>
+          </SelectHighwayModal.ContainerField>
+        </SelectHighwayModal.ModalContainer>
+      </SelectHighwayModal.Root>
       <div className="container relative mx-auto grid min-h-screen grid-rows-[60px,auto,70px] px-6 pb-8 pt-4">
         <Header.Root>
           <Header.NavLinksContainer>
@@ -62,50 +107,7 @@ export default function Trechos({ highways }: Props) {
             />
           </Header.NavLinksContainer>
         </Header.Root>
-        <SelectHighwayModal.Root show>
-          <SelectHighwayModal.Mask />
-          <SelectHighwayModal.ModalContainer>
-            <SelectHighwayModal.Title>
-              Rodovia a ser Mapeada
-            </SelectHighwayModal.Title>
-            <SelectHighwayModal.ContainerField>
-              <label htmlFor="">Nome da Rodovia</label>
-              <select
-                placeholder="Selecione"
-                className="my-2 w-96 rounded-md border bg-white px-2 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                onChange={handleOnChangeSelectedHighway}
-              >
-                <option value="" hidden defaultValue="true">
-                  Selecione uma opção
-                </option>
-                {highways.map(highway => (
-                  <option key={highway.id} value={highway.id}>
-                    {highway.name}
-                  </option>
-                ))}
-              </select>
-            </SelectHighwayModal.ContainerField>
-            <SelectHighwayModal.ContainerField>
-              <FormSearch.Button
-                type="button"
-                disabled={!hasHighwaySelected}
-                className="
-                disabled:focus-ring-white
-                  mx-0 flex
-                justify-center bg-green-600
-                text-white
-                  hover:bg-green-500 focus:ring-green-600
-                disabled:bg-green-300
-                disabled:hover:bg-green-300
-                  sm:mx-0
-                "
-                onClick={handleToInitializeMapping}
-              >
-                Começar a Mapear
-              </FormSearch.Button>
-            </SelectHighwayModal.ContainerField>
-          </SelectHighwayModal.ModalContainer>
-        </SelectHighwayModal.Root>
+
         <section className="flex flex-1 flex-col items-start p-3">
           <div className="flex py-2">
             <h1 className="max-w-md text-4xl font-medium text-gray-700">
