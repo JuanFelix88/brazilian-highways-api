e6228a1f561ae8d09a0f7b5e092ad61bda1ba0a9
Juan Felix <andrade.juan@outlook.com.br>
2023-03-07 20:27:44 -0300

    feat: add search by position proximity in keypointers controller

--- pages/api/keypointers.ts
+++ pages/api/keypointers.ts
@@ -1,4 +1,5 @@
 import { FindPointers } from '@/src/application/use-cases/find-pointers'
+import { FindPointersByPositionProximity } from '@/src/application/use-cases/find-pointers-by-position-proximity'
 import { DataMappingService } from '@/src/infra/data-mapping/data-mapping.service'
 import { DataMappingPointersRepository } from '@/src/infra/data-mapping/repositories/data-mapping-pointers-repository'
 import { Controller } from '@/src/utils/controller'
@@ -7,24 +8,55 @@ export namespace KeypointersController {
   export namespace GET {
     export interface Params {
       q?: string
+      position?: string
+      ray_proximity?: string
     }
   }
 }
 
 export class KeypointersController extends Controller {
-  constructor(private readonly findPointers: FindPointers) {
+  constructor(
+    private readonly findPointers: FindPointers,
+    private readonly findPointersByPositionProximity: FindPointersByPositionProximity
+  ) {
     super()
   }
 
   async get(req: Controller.Request, res: Controller.Response) {
     try {
-      const { q: search } = req.query as KeypointersController.GET.Params
+      const {
+        q: search,
+        position,
+        ray_proximity: rayProximity = '1500'
+      } = req.query as KeypointersController.GET.Params
 
-      const { pointers: keypointers } = await this.findPointers.execute({
-        search
+      const isSearch = !!search
+      const isPositionProximitySearch = !!position
+
+      if (isSearch) {
+        const { pointers: keypointers } = await this.findPointers.execute({
+          search
+        })
+
+        return res.status(200).send(keypointers)
+      }
+
+      if (isPositionProximitySearch) {
+        const { pointers: keypointers } =
+          await this.findPointersByPositionProximity.execute({
+            position: position.split(',').map(latlong => Number(latlong)) as [
+              number,
+              number
+            ],
+            rayProximity: Number(rayProximity)
+          })
+
+        return res.status(200).send(keypointers)
+      }
+
+      return res.status(400).send({
+        error: 'no search method has been defined in query params'
       })
-
-      res.status(200).send(keypointers)
     } catch (err) {
       if (err instanceof Error && err.message === 'no keypointer was found') {
         return res.status(404).send({
@@ -40,5 +72,6 @@ export class KeypointersController extends Controller {
 export default KeypointersController.defineHandlerWithDependencyInjects([
   FindPointers,
   DataMappingPointersRepository,
-  DataMappingService
+  DataMappingService,
+  FindPointersByPositionProximity
 ])
