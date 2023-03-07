430003fa9dae5b0aae761804d2f31ee8086621fe
Juan Felix <andrade.juan@outlook.com.br>
2023-03-06 23:28:46 -0300

    chore: implements find method in data-mapping-pointers-repository

--- src/infra/data-mapping/repositories/data-mapping-pointers-repository.ts
+++ src/infra/data-mapping/repositories/data-mapping-pointers-repository.ts
@@ -1,7 +1,11 @@
+import { Highway } from '@/src/application/entities/highway'
 import { KeyPointer } from '@/src/application/entities/key-pointer'
+import { KilometerMarker } from '@/src/application/entities/kilometer-marker'
 import { Pointer } from '@/src/application/entities/pointer'
 import { PointersRepository } from '@/src/application/repositories/pointers-repository'
 import { DataMappingService, Filenames } from '../data-mapping.service'
+import MiniSearch, { Query, SearchResult } from 'minisearch'
+import removeAccents from 'remove-accents'
 
 export class DataMappingPointersRepository implements PointersRepository {
   constructor(private readonly dataMappingService: DataMappingService) {}
@@ -15,6 +19,72 @@ export class DataMappingPointersRepository implements PointersRepository {
     return {
       id: (pointer as any).id
     }
+  }
+
+  async find({
+    highwayId,
+    search
+  }: {
+    highwayId?: number
+    search?: string
+  }): Promise<KilometerMarker[]> {
+    const keypointers: KeyPointer[] =
+      await this.dataMappingService.loadFromName(Filenames.KeyPointers)
+
+    const miniSearch = new MiniSearch<KeyPointer>({
+      fields: ['km', 'marker', 'city', 'uf', 'rodId'] as Array<
+        keyof KeyPointer
+      >,
+      idField: 'position',
+      storeFields: [
+        'accuracyInMeters',
+        'city',
+        'direction',
+        'km',
+        'marker',
+        'position',
+        'rodId',
+        'uf'
+      ] as Array<keyof KeyPointer>,
+      processTerm: term => removeAccents(term.toString().toLowerCase()),
+      searchOptions: {
+        processTerm: term => removeAccents(term.toString().toLowerCase())
+      }
+    })
+
+    miniSearch.addAll(keypointers)
+
+    const filteredKeypointersList: Array<SearchResult & KeyPointer> =
+      miniSearch.search({
+        combineWith: 'AND',
+        queries: [
+          {
+            queries: [highwayId?.toString() ?? ''],
+            fields: ['rodId']
+          },
+          search && {
+            combineWith: 'OR',
+            prefix: true,
+            queries: [
+              {
+                queries: [search.toString()],
+                fields: ['km', 'marker', 'city', 'uf']
+              }
+            ]
+          }
+        ].filter(item => Boolean(item)) as Query[]
+      }) as []
+
+    return filteredKeypointersList.map(keypointer => ({
+      accuracyInMeters: keypointer.accuracyInMeters,
+      city: keypointer.city,
+      direction: keypointer.direction,
+      km: keypointer.km,
+      marker: keypointer.marker,
+      position: keypointer.position,
+      rodId: keypointer.rodId,
+      uf: keypointer.uf
+    }))
   }
 
   async findByHighwayId(highwayId: number): Promise<KeyPointer[]> {
