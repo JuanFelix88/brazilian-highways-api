4de92a2237c1fc15caac446f0f42f96aea01a67d
Juan Felix <andrade.juan@outlook.com.br>
2023-03-20 21:53:44 -0300

    test: improve make-pointers-factory locations to use in position proximity tests

--- test/factories/make-pointers-factory.ts
+++ test/factories/make-pointers-factory.ts
@@ -8,7 +8,7 @@ const pointers: KeyPointer[] = [
     id: 2131,
     km: 22,
     marker: 21,
-    position: [-23.14442, -44.1234],
+    position: [-25.519864, -48.543768],
     rodId: 0,
     uf: 'PR'
   },
@@ -19,7 +19,7 @@ const pointers: KeyPointer[] = [
     id: 4215,
     km: 23,
     marker: 22,
-    position: [-23.14442, -44.1234],
+    position: [-25.527693, -48.547637],
     rodId: 0,
     uf: 'PR'
   },
@@ -30,7 +30,7 @@ const pointers: KeyPointer[] = [
     id: 1114,
     km: 24,
     marker: 23,
-    position: [-23.14442, -44.1234],
+    position: [-25.534421219456927, -48.55378969468281],
     rodId: 0,
     uf: 'PR'
   },
@@ -41,7 +41,7 @@ const pointers: KeyPointer[] = [
     id: 25412,
     km: 25,
     marker: 24,
-    position: [-23.14442, -44.1234],
+    position: [-25.541805, -48.559019],
     rodId: 0,
     uf: 'PR'
   },
@@ -52,7 +52,7 @@ const pointers: KeyPointer[] = [
     id: 7751,
     km: 26,
     marker: 25,
-    position: [-23.14442, -44.1234],
+    position: [-25.550453, -48.561377],
     rodId: 0,
     uf: 'PR'
   }
