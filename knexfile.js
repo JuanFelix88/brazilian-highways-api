02f7c0bb6d7172f36e1cade0101780893a56ee77
Juan Felix <andrade.juan@outlook.com.br>
2023-08-23 23:26:59 -0300

    chore: add knexfile.js to config knex connections

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user:     process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+};
